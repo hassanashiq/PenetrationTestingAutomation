341b2205919e065fc4408424163f665e8cabefea
Hassan Ashiq <38008460+hassanashiq@users.noreply.github.com>
2022-06-13 10:26:26 +0500

    Update amassautomated.sh

--- AmassAutomated/amassautomated.sh
+++ AmassAutomated/amassautomated.sh
@@ -12,5 +12,8 @@ fi
 
 echo "[+] Harvesting subdomains with Amass..."
 assetfinder $1 >> $1/recon/f.txt
-sort -u $1/recon/f.txt >> $1/recon/f.txt
+sort -u $1/recon/f.txt >> $1/recon/final.txt
 rm $1/recon/f.txt
+
+echo "[+] Probing for alive domains..."
+cat $1/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $1/recon/alive.txt
