bd6ea2453062bce7c02ecbd9ed847b14ea345567
Hassan Ashiq <38008460+hassanashiq@users.noreply.github.com>
2022-06-13 10:39:38 +0500

    Update AssetFinderAutomated.sh

--- AssetFinderAutomated/AssetFinderAutomated.sh
+++ AssetFinderAutomated/AssetFinderAutomated.sh
@@ -13,5 +13,7 @@ fi
 echo "[+] Harvesting subdomains with Assetfinder..."
 assetfinder $1 >> $1/recon/assets.txt
 cat $1/recon/assets.txt | grep $1 >> $1/recon/final.txt
-
 rm $1/recon/assets.txt
+
+echo "[+] Probing for alive domains..."
+cat $1/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $1/recon/alive.txt
